1597b77820fae0ba884005ae172dbdafd25dcc6f
Serguxa9999 <svp.perepechaev03@gmail.com>
2021-06-03 00:29:06 +0300

    Syroi proect

---
+++ MusicBox/bot/commands/tic-tac-toe.py
@@ -0,0 +1,118 @@
+"""
+import discord
+from discord.ext import commands
+
+import asyncio
+
+@commands.command(name='tic-tac-toe', description=config.HELP_YT_LONG, help=config.HELP_YT_SHORT, aliases=['tictactoe', 'ttt', 'playttt'])
+    async def _play_song(self, ctx, *, track: str):
+
+        if(await utils.is_connected(ctx) == None):
+            await General.uconnect(self, ctx)
+        if track.isspace() or not track:
+            return
+
+        if await utils.play_check(ctx) == False:
+            return
+
+        current_guild = utils.get_guild(self.bot, ctx.message)
+        audiocontroller = utils.guild_to_audiocontroller[current_guild]
+
+        if audiocontroller.playlist.loop == True:
+            await ctx.send("Loop is enabled! Use {}loop to disable".format(config.BOT_PREFIX))
+            return
+
+        song = await audiocontroller.process_song(track)
+
+        if song is None:
+            await ctx.send(config.SONGINFO_UNKNOWN_SITE)
+            return
+
+        if song.origin == linkutils.Origins.Default:
+
+            if audiocontroller.current_song != None and len(audiocontroller.playlist.playque) == 0:
+                await ctx.send(embed=song.info.format_output(config.SONGINFO_NOW_PLAYING))
+            else:
+                await ctx.send(embed=song.info.format_output(config.SONGINFO_QUEUE_ADDED))
+
+        elif song.origin == linkutils.Origins.Playlist:
+            await ctx.send(config.SONGINFO_PLAYLIST_QUEUED)
+from tkinter import *
+import random
+root = Tk()
+root.title('Criss-cross')
+game_run = True
+field = []
+cross_count = 0
+
+def new_game():
+    for row in range(3):
+        for col in range(3):
+            field[row][col]['text'] = ' '
+            field[row][col]['background'] = 'lavender'
+    global game_run
+    game_run = True
+    global cross_count
+    cross_count = 0
+
+def click(row, col):
+    if game_run and field[row][col]['text'] == ' ':
+        field[row][col]['text'] = 'X'
+        global cross_count
+        cross_count += 1
+        check_win('X')
+        if game_run and cross_count < 5:
+            computer_move()
+            check_win('O')
+
+def check_win(smb):
+    for n in range(3):
+        check_line(field[n][0], field[n][1], field[n][2], smb)
+        check_line(field[0][n], field[1][n], field[2][n], smb)
+    check_line(field[0][0], field[1][1], field[2][2], smb)
+    check_line(field[2][0], field[1][1], field[0][2], smb)
+
+def check_line(a1,a2,a3,smb):
+    if a1['text'] == smb and a2['text'] == smb and a3['text'] == smb:
+        a1['background'] = a2['background'] = a3['background'] = 'pink'
+        global game_run
+        game_run = False
+
+def can_win(a1,a2,a3,smb):
+    res = False
+    if a1['text'] == smb and a2['text'] == smb and a3['text'] == ' ':
+        a3['text'] = 'O'
+        res = True
+    if a1['text'] == smb and a2['text'] == ' ' and a3['text'] == smb:
+        a2['text'] = 'O'
+        res = True
+    if a1['text'] == ' ' and a2['text'] == smb and a3['text'] == smb:
+        a1['text'] = 'O'
+        res = True
+    return res
+
+def computer_move():
+    for n in range(3):
+        if can_win(field[n][0], field[n][1], field[n][2], 'O'):
+            return
+        if can_win(field[0][n], field[1][n], field[2][n], 'O'):
+            return
+    if can_win(field[0][0], field[1][1], field[2][2], 'O'):
+        return
+    if can_win(field[2][0], field[1][1], field[0][2], 'O'):
+        return
+    for n in range(3):
+        if can_win(field[n][0], field[n][1], field[n][2], 'X'):
+            return
+        if can_win(field[0][n], field[1][n], field[2][n], 'X'):
+            return
+    if can_win(field[0][0], field[1][1], field[2][2], 'X'):
+        return
+    if can_win(field[2][0], field[1][1], field[0][2], 'X'):
+        return
+    while True:
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+        if field[row][col]['text'] == ' ':
+            field[row][col]['text'] = 'O'
+            break
